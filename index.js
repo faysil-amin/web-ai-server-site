e6e304c0f40d05b4e02d54e43d4cc2230e5805c0
faysil-amin <foodbrochandpur@gmail.com>
2026-02-17 12:40:26 +0600

    add delete-add-model

--- index.js
+++ index.js
@@ -3,9 +3,35 @@ require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 3000;
 const cors = require("cors");
+const admin = require("firebase-admin");
+
+const serviceAccount = require("./assignment-10-72af4-firebase-adminsdk-fbsvc-5e7bc47e2c.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
 app.use(cors());
 app.use(express.json());
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const verifation = async (req, res, next) => {
+  const userHeader = req.headers.authorization;
+  if (!userHeader) {
+    return res.status(401).send({ message: `unauthorize user` });
+  }
+  const token = req.headers.authorization.split(" ")[1];
+  if (!token) {
+    return res.status(401).send({ message: `unauthorize user` });
+  }
+  try {
+    const check = await admin.auth().verifyIdToken(token);
+    req.token_email = check.email;
+    d;
+    next();
+  } catch (error) {
+    return res.status(401).send({ message: `unauthorize user` });
+  }
+};
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rgj7zze.mongodb.net/?appName=Cluster0`;
 const client = new MongoClient(uri, {
   serverApi: {
@@ -20,9 +46,25 @@ async function run() {
     await client.connect();
     const aiModel = client.db("aiModel");
     const model = aiModel.collection("model");
+    app.get("/add-model", async (req, res) => {
+      const cursor = model.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+    app.get("/add-latest-model", async (req, res) => {
+      const cursor = model.find().limit(6);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
     app.post("/add-model", async (req, res) => {
       const cursor = req.body;
       const result = await model.insertOne(cursor);
+      res.send(result);
+    });
+    app.delete("/add-model/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await model.deleteOne(query);
       res.send(result);
     });
     await client.db("admin").command({ ping: 1 });
